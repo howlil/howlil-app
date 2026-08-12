93c14dd7180777867a5431c26f558e4c75a654cf
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-08-13 03:46:53 +0700

    chore: remove legacy portfolio scaffolding

--- src/components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-export { default as ExternalLink } from './ExternalLink';
-export { default as Tag } from './Tag';
-export { default as Card } from './Card';
-export { default as Button } from './Button';
-export { default as Modal } from './Modal';
