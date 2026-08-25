02d9492bc88ff62f5bf1d5b79298ea1083d16f5e
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-08-25 14:06:50 +0700

    refactor: simplify navigation labels

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -9,9 +9,9 @@ export interface NavLink {
 
 export const NAV_LINKS: NavLink[] = [
   { name: 'Home', href: withBase('/') },
+  { name: 'Work', href: withBase('/projects') },
   { name: 'About', href: withBase('/about') },
-  { name: 'Project', href: withBase('/projects') },
-  { name: 'Blog', href: withBase('/blog') },
+  { name: 'Writing', href: withBase('/blog') },
   {
     name: 'Resume',
     href: 'https://drive.google.com/file/d/1fvp5VfE-dk3-HzG6vbxfHvEEvAiBkHPQ/view?usp=sharing',
