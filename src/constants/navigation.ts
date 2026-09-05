30ce78b949b309fdb3e88b72f07c151767f6788e
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-09-06 04:27:24 +0700

    rename work navigation to projects

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -9,9 +9,9 @@ export interface NavLink {
 
 export const NAV_LINKS: NavLink[] = [
   { name: 'Home', href: withBase('/') },
-  { name: 'Work', href: withBase('/projects') },
-  { name: 'About', href: withBase('/about') },
+  { name: 'Projects', href: withBase('/projects') },
   { name: 'Writing', href: withBase('/blog') },
+  { name: 'About', href: withBase('/about') },
   {
     name: 'Resume',
     href: 'https://drive.google.com/file/d/1fvp5VfE-dk3-HzG6vbxfHvEEvAiBkHPQ/view?usp=sharing',
